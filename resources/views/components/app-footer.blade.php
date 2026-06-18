4ee4467d533bb764ebf38645bfb473cb13ed4811
Sinan <logistus@gmail.com>
2026-06-18 20:33:57 +0300

    footer revised

--- resources/views/components/app-footer.blade.php
+++ resources/views/components/app-footer.blade.php
@@ -5,8 +5,8 @@
 @endphp
 
 @if ($flux)
-    <flux:footer container class="{{ $classes }}">
-        <div class="flex flex-col gap-3 text-sm text-zinc-500 sm:flex-row sm:items-center sm:justify-between dark:text-zinc-400">
+    <footer class="{{ $classes }}">
+        <div class="container mx-auto flex flex-col gap-3 py-6 text-sm text-zinc-500 sm:flex-row sm:items-center sm:justify-between dark:text-zinc-400">
             <p>&copy; {{ now()->year }} {{ config('app.name', 'HitsTrack') }}. {{ __('All rights reserved.') }}</p>
 
             <nav class="flex gap-4">
@@ -15,10 +15,10 @@
                 </flux:link>
             </nav>
         </div>
-    </flux:footer>
+    </footer>
 @else
     <footer class="{{ $classes }}">
-        <div class="mx-auto flex max-w-7xl flex-col gap-3 px-6 py-6 text-sm text-zinc-500 sm:flex-row sm:items-center sm:justify-between lg:px-8 dark:text-zinc-400">
+        <div class="container mx-auto flex flex-col gap-3 py-6 text-sm text-zinc-500 sm:flex-row sm:items-center sm:justify-between dark:text-zinc-400">
             <p>&copy; {{ now()->year }} {{ config('app.name', 'HitsTrack') }}. {{ __('All rights reserved.') }}</p>
 
             <nav class="flex gap-4">
