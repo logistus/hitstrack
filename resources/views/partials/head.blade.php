49c311cf47d1bd0e05ec84dd78e7bf35c1de46ab
Sinan <logistus@gmail.com>
2026-06-23 00:07:43 +0300

    google analytics

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,3 +1,15 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2LXRWHC1C4"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-2LXRWHC1C4');
+</script>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
